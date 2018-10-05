555b58c8b9b8e564d7ed8475a07d90b10a8a4e49
joel <zorcey@gmail.com>
2018-10-05 21:32:52 +0200

    Implementing connect in DatabaseConnection

--- model/DatabaseConnection.php
+++ model/DatabaseConnection.php
@@ -1,15 +1,23 @@
 <?php
-    class DatabaseConnection {
+  /**
+   * Class holds database connection
+   * Source of code: https://www.youtube.com/watch?v=bjT5PJn0Mu8
+   */
+  class DatabaseConnection {
+    /**
+     * Creates a database connection
+     * @return PDO Database Connection
+     */
     public function connect () {
       $server = "localhost";
-      $database = "users";
-      $username = "asd";
-      $password = "asd";
+      $database = "user_data";
+      $username = "root";
+      $password = "root";
       try {
-			 return new PDO("mysql:host=$server;dbname=$database;", $user, $pass);
+			 return new PDO("mysql:host=$server;dbname=$database;", $username, $password);
 		  }
 		  catch(PDOException $e) {
-			  return $e->getMessage();
+			  die ("Connection Failed: " . $e->getMessage());
       }
     }
   }
