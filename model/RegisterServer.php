0b9e850ca60436c99756a7ab277aac38d37807cc
joel <zorcey@gmail.com>
2018-10-06 08:09:03 +0200

    Ading functionality to RegisterServer

--- model/RegisterServer.php
+++ model/RegisterServer.php
@@ -1,20 +1,29 @@
 <?php
 namespace model;
 
-require '/model/DatabaseConnection.php';
 use Exception;
 
-class LoginServer {
+class RegisterServer {
 
     private $dc;
     public function __constructor (\model\DatabaseConnection $dc) {
-        $this->dc = $dc;
+        $this->dc = $dc; 
     }
     public function registerNewUser($user) {
         $this->addNewUserToDatabase($user);
     }
     private function addNewUserToDatabase($user) {
+        $username = 'testUser';
+        $password = 'pswrd';
         $sql = "INSERT INTO users (username, password) VALUES (:username, :password)";
-        $statement = $conn->prepare($sql);
+        $statement = $this->dc->connect()->prepare($sql);
+        $statement->bindParam(':username', $username);
+        $statement->bindParam(':password', $password);
+
+        if ($statement->execute()) {
+            die ('Success');
+        } else {
+            die ('Fail');
+        }
     }
 }
