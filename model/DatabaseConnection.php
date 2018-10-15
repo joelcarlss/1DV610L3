f97716920c37c66f2556dbdd73e0b990ef40c4ea
joel <zorcey@gmail.com>
2018-10-15 19:16:23 +0200

    Changing variables to consts from databaseValue, adding DatabaseConnection to GH

---
+++ model/DatabaseConnection.php
@@ -0,0 +1,30 @@
+<?php
+namespace model;
+use Exception;
+use PDO;
+
+  require_once('model/databaseValues.php');
+  /**
+   * Class holds database connection
+   * Source of code: https://www.youtube.com/watch?v=bjT5PJn0Mu8
+   */
+  class DatabaseConnection {
+    /**
+     * Creates a database connection
+     * @return PDO Database Connection
+     */
+    public function connect () {
+      $server = SERVER_ADRESS;
+      $database = DATABASE;
+      $username = DB_USERNAME;
+      $password = DB_PASSWORD;
+
+      try {
+        return $conn = new PDO("mysql:host=$server;dbname=$database;", $username, $password);
+			//  return new PDO("mysql:host=" . $this->server . ";dbname=" . $this->database . ";", $this->username, $this->password);
+		  }
+		  catch(PDOException $e) {
+			  die ("Connection Failed: " . $e->getMessage());
+      }
+    }
+  }
