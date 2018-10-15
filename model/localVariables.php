a224c5c4d2e8d2198f266020446c3d40f90a4cb4
joel <zorcey@gmail.com>
2018-10-15 17:10:59 +0200

    Updating consts

--- model/localVariables.php
+++ model/localVariables.php
@@ -1,4 +1,10 @@
-    // $server = "192.168.64.2";
-      // $database = "auth";
-      // $username = "asd";
-      // $password = 'asd';
+<?php     
+    $server = "192.168.64.2";
+    $database = "auth";
+    $username = "asd";
+    $password = 'asd';
+
+    const SERVER_ADRESS = "192.168.64.2";
+    const DATABASE = "auth";
+    const DB_USERNAME = "asd";
+    CONST DB_PASSWORD = "asd";
